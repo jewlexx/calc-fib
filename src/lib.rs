526a51133d5d0c90458cbdefba6dec2d5995b715
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 09:39:02 +1000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,16 @@ pub fn calc_fib(n: usize) -> BigUint {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
+    use num_bigint::BigUint;
+
+    use super::*;
+
     #[test]
     fn test_get_500th() {
-        // let nth500 = 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125u128;
+        let nth500 = BigUint::from_str("139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125").unwrap();
+
+        assert_eq!(calc_fib(500), nth500);
     }
 }
