9b16001f583cafe752321994f668209809aa9245
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 09:37:47 +1000

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
-pub fn calc_fib(n: usize) -> u128 {
-    let mut numbers = [1u128, 1u128];
+use num_bigint::{BigUint, ToBigUint};
+
+pub fn calc_fib(n: usize) -> BigUint {
+    let mut numbers = vec![1u8.to_biguint().unwrap(), 1u8.to_biguint().unwrap()];
 
     for _ in 0..=n {
-        let old_x = numbers[0];
-        let old_y = numbers[1];
-        numbers[0] = old_y;
-        numbers[1] = old_x + old_y;
+        numbers.push(numbers.get(0).unwrap() + numbers.get(1).unwrap());
+        numbers.remove(0);
     }
 
-    numbers[1]
+    numbers.remove(1)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_get_500th() {
-        let nth500 = 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125u128;
+        // let nth500 = 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125u128;
     }
 }
