94bff2fa036384766cf30bf41b761b7e84be208d
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 10:53:14 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::str::FromStr;
+
+use num_bigint::BigUint;
+
 fn main() {
     let arg = std::env::args().nth(1).unwrap_or_else(|| {
         println!("Please enter a number");
+        panic!();
+    });
+
+    BigUint::from_str(&arg).unwrap_or_else(|_| {
+        println!("Please pass a valid number");
         panic!();
     });
 
